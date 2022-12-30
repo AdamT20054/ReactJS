e53626a031518d661c36aa2ff0c10c7f18098048
AdamT20054 <@AdamThomasOneill200547>
2022-12-30 01:30:21 +0000

    IDs mean that when a SPECIFIC element in the monsters array updates, the ID associated with the new name value is the ONLY one that will be re-rendered since it will find the key given in the div and update that one specifically.
    React is the only thing that needs the key value, it's removed in HTML but is kept under the hood to track values.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,40 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+
+
+
 class App extends Component {
     constructor() {
         super();
         this.state = {
             monsters: [
                 {
-                name: 'Linda'
-                },
-                {
-                name: 'Frank'
-                },
-                {
-                name: 'Jackie'
-                },
-                {
-                    name: 'Adam'
-                }]
+                name: 'Linda',
+                id: '1'
+                }, {
+                name: 'Frank',
+                id: '2'
+                }, {
+                name: 'Jackie',
+                id: '3'
+                }, {
+                name: 'Adam',
+                id: '4'
+                }
+            ]
         }
     }
 
-  render() {
-    return (
-        <div className="App">
-            {
-                this.state.monsters.map((monster) => {
-                    return <h1>{monster.name}</h1>
-                })
-            }
-
-
-        </div>
-    );
-  }
-}
+    render() {
+        return (
+            <div className="App">
+                {
+                    this.state.monsters.map((monster) => {
+                        return (
+                            <div key={monster.id}>
+                                <h1>{monster.name}</h1>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        );
+      }
+    }
 
 
 export default App;
