824fe385661a95ec99dfb8ac2676005c642af6ff
AdamT20054 <Git@AdamT05.com>
2023-08-13 03:10:50 +0100

    Mapped monsters array onto headings.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,31 +1,37 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            name: 'Adam'
+             monsters: [
+                 {
+                    name: 'Linda',
+                },
+                {
+                    name: 'Frank',
+                },
+                {
+                    name: 'Jacky',
+                },
+                {
+                    name: 'Bobby',
+                }
+             ]
         }
     }
 
   render() {
     return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Hello, {this.state.name}!
-
-            </p>
-            <button onClick={
-                () => {
-                    this.setState({name: 'Bobby'})
-                }
-            }>Change Name</button>
-          </header>
+            <h1>Monsters Rolodex</h1>
+            {
+                this.state.monsters.map(monster => {
+                    return <h1>{monster.name}</h1>
+                })
+            }
         </div>
     );
   }
--- monsters-rolodex/src/dev/useInitial.js
+++ monsters-rolodex/src/dev/useInitial.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 export const useInitial = () => {
+    // eslint-disable-next-line
     const [status, setStatus] = useState({
         loading: false,
         error: false
