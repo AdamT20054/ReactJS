281fc6f783f9471ebf572f91a3b7a6a3720bf8b2
AdamT20054 <Git@AdamT05.com>
2023-08-13 04:10:31 +0100

    MY own implementation of the search & filtering for Rolodex.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,16 +6,20 @@ class App extends Component {
   constructor() { // Runs First
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      Searchmonsters: []
     }
+    console.log("Constructor")
   }
 
   // Runs after the initial UI has been rendered (mounted) to the DOM
   componentDidMount() {
+    console.log("ComponentDidMount")
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState(() => {
-        return { monsters: users }
+        return { monsters: users,
+                 Searchmonsters: users}
       }, () => {
         console.log(this.state.monsters)
       }))
@@ -23,11 +27,19 @@ class App extends Component {
 
   // Runs Second, mounts initial UI state to the DOM. Does not mount again, just updates.
   render() {
+    console.log("Render")
     return (
       <div className='App'>
+        <input className='search-box' type='search' placeholder='Search Monsters' onChange={(event) => {
+          this.setState((state) => {
+            return { Searchmonsters: state.monsters.filter(monster => monster.name.toLowerCase().includes(event.target.value.toLowerCase()))}
+          }, () => {
+            console.log(event.target.value)
+          })
+        }}/>
         <h1>Monsters Rolodex</h1>
         {
-          this.state.monsters.map(monster => {
+          this.state.Searchmonsters.map(monster => {
             return <div key={monster.id}>
               <h2>{monster.name}</h2>
             </div>
